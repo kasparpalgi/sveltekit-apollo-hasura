ea753da3e75b69623536eb8cce9be1a4ea530c18
Kaspar L. Palgi <git@e-stonia.co.uk>
2023-06-17 13:55:06 +0300

    Display messages

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# SvelteKit & Hasura subscriptions with Apollo
+
+https://hasura.io/learn/graphql/svelte-apollo
+
 # create-svelte
 
 Everything you need to build a Svelte project, powered by [`create-svelte`](https://github.com/sveltejs/kit/tree/master/packages/create-svelte).
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,53 +1,82 @@
 <script>
-    import { ApolloClient, InMemoryCache, HttpLink, gql } from '@apollo/client/core';
-    import { setClient, query } from 'svelte-apollo';
-  
-    const hasuraAdminSecret = 'woofi2023#hasura';
-  
-    function createApolloClient() {
-      const link = new HttpLink({
-        uri: 'https://api.woofi.ai/v1/graphql',
-        headers: {
-          'X-Hasura-Admin-Secret': hasuraAdminSecret
-        }
-      });
-  
-      const cache = new InMemoryCache();
-  
-      return new ApolloClient({
-        link,
-        cache
-      });
-    }
-  
-    const GET_MESSAGES = gql`
-    query MyMessages {
-      chat_messages(where: {_or: {receiver_id: {_eq: 1}, sender_id: {_eq: 1}}}, order_by: {date_created: desc}) {
-        user {
-          first_name
-        }
-        userBySenderId {
-          first_name
-        }
-        group {
-          group_name
-        }
-        message_text
-        date_created
-        date_updated
-      }
-    }
-    `;
-  
-    const client = createApolloClient();
-    setClient(client);
-    const messages = query(GET_MESSAGES);
-  
-    $: console.log($messages);
-  </script>
-  
-  
+	import { ApolloClient, InMemoryCache, HttpLink, gql } from '@apollo/client/core';
+	import { setClient, query } from 'svelte-apollo';
 
+	const hasuraAdminSecret = 'your_hasura_admin_secret'; // Place this in ENV
+	let chatMessages;
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	$: {
+		if ($messages.data && $messages.data.chat_messages) {
+			chatMessages = $messages.data.chat_messages;
+		}
+	}
+
+	function createApolloClient() {
+		const link = new HttpLink({
+			uri: 'https://api.yourdomain.com/v1/graphql', // Add your Hasura endpoint
+			headers: {
+				'X-Hasura-Admin-Secret': hasuraAdminSecret
+			}
+		});
+
+		const cache = new InMemoryCache();
+
+		return new ApolloClient({
+			link,
+			cache
+		});
+	}
+
+	const GET_MESSAGES = gql`
+		query MyMessages {
+			chat_messages(
+				where: { _or: { receiver_id: { _eq: 1 }, sender_id: { _eq: 1 } } }
+				order_by: { date_created: desc }
+			) {
+				user {
+					first_name
+				}
+				userBySenderId {
+					first_name
+				}
+				group {
+					group_name
+				}
+				message_text
+				date_created
+				date_updated
+			}
+		}
+	`;
+
+	const client = createApolloClient();
+	setClient(client);
+	const messages = query(GET_MESSAGES);
+
+	$: console.log($messages);
+</script>
+
+<h1>Messages</h1>
+<div class="chat-container">
+	{#if $messages.loading}
+		<p>Loading...</p>
+	{:else if chatMessages}
+		{#each chatMessages as message (message.date_created)}
+			<div class="chat-message">
+				<p>
+					<strong>{message.user.first_name}:</strong>
+					<span>{message.message_text}</span>
+				</p>
+				<p>
+					<small>Created at: {message.date_created}</small>
+					<small>Updated at: {message.date_updated}</small>
+				</p>
+			</div>
+		{/each}
+	{:else}
+		<p>No messages found</p>
+	{/if}
+	{#if $messages.error}
+		<p>Something went wrong: {$messages.error.message}</p>
+	{/if}
+</div>
