a6057de30091fb0d66ccd2319ad8a0db4391c9c9
Kaspar L. Palgi <git@e-stonia.co.uk>
2023-06-19 09:57:13 +0300

    Get only messages where the user is sender, receiver or group member

--- src/routes/subscriptions/+page.svelte
+++ src/routes/subscriptions/+page.svelte
@@ -18,6 +18,7 @@
 	const GRAPHQL_URI = 'https://api.yourdomain.com/v1/graphql';
 	const WS_URI = 'wss://api.yourdomain.com/v1/graphql';
 	const BATCH_SIZE = 10;
+	const USER_ID = 1; // Change it to your actual user id
 
 	const cache = new InMemoryCache({
 		typePolicies: {
@@ -59,25 +60,47 @@
 	);
 
 	const client = new ApolloClient({ link, cache });
-	const query = gql`
-		subscription GetChatMessagesStreamingSubscription {
-			chat_messages_stream(batch_size: ${BATCH_SIZE}, cursor: { initial_value: { id: 0 } }) {
-				group_id
-				id
-				receiver_id
-				sender_id
-				message_text
-				date_created
-				date_updated
-			}
-		}
-	`;
+
+	const GET_CHAT_MESSAGES_SUBSCRIPTION = gql`
+        subscription GetChatMessagesStreamingSubscription($user: Int!) {
+            chat_messages_stream(
+                batch_size: ${BATCH_SIZE}, 
+                cursor: { initial_value: { id: 0 } },
+                where: { 
+                    _or: [
+                        {sender_id: {_eq: $user}}, 
+                        {receiver_id: {_eq: $user}},
+                        {group: {users_groups: {users_id: {_eq: $user}}}}
+                    ], 
+                }) {
+                id
+                message_text
+                date_created
+                date_updated
+                user {
+                    first_name
+                    profile_pic
+                }
+                userBySenderId {
+                    first_name
+                    profile_pic
+                }
+                group {
+                    group_name
+                    pic_url
+                }
+            }
+        }
+    `;
 
 	let chatMessages = [];
-	const subscription = client.subscribe({ query }).subscribe({
-		next: ({ data }) => data && (chatMessages = data.chat_messages_stream),
-		error: (err) => console.error('Subscription error', err)
-	});
+
+	const subscription = client
+		.subscribe({ query: GET_CHAT_MESSAGES_SUBSCRIPTION, variables: { user: USER_ID } })
+		.subscribe({
+			next: ({ data }) => data && (chatMessages = data.chat_messages_stream),
+			error: (err) => console.error('Subscription error', err)
+		});
 
 	onDestroy(() => subscription.unsubscribe());
 </script>
@@ -86,8 +109,13 @@
 <div class="chat-container">
 	{#each chatMessages as message (message.id)}
 		<div class="chat-message">
-			<p><strong>Sender:</strong> {message.sender_id}</p>
-			<p><strong>Receiver:</strong> {message.receiver_id}</p>
+			<p><strong>Sender:</strong> {message.userBySenderId?.first_name}</p>
+			{#if message.user?.first_name}
+				<p><strong>Receiver:</strong> {message.user?.first_name}</p>
+			{/if}
+			{#if message.group?.group_name}
+				<p><strong>Group:</strong> {message.group?.group_name}</p>
+			{/if}
 			<p><strong>Message:</strong> {message.message_text}</p>
 			<p><strong>Date created:</strong> {message.date_created}</p>
 			<p><strong>Date updated:</strong> {message.date_updated}</p>
